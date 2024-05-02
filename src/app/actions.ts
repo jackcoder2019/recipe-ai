5cefd353dfa88f5007a02598e97b9f86db64a2b3
Jielin Li <jielinli@Jielins-MacBook-Pro.local>
2024-05-01 22:39:35 -0700

    dummy commit

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,7 +4,7 @@ export async function generateRecipe(formData: FormData) {
   const response = await amplifyClient.queries.askBedrock({
     ingredients: [formData.get("ingredients")?.toString() || ""],
   });
-
+ 
   const res = JSON.parse(response.data?.body!);
   const content = res.content[0].text;
   return content || "";
